65ebc4f8c414b928c8fdd7b5af237fa7df823a01
Alexander Vykhodtsev <a.a.vykhodtsev@gmail.com>
2026-02-13 16:57:07 +0600

    Now it is possible to override any setting with command line param

--- source/fsc.py
+++ source/fsc.py
@@ -24,7 +24,7 @@ default_settings = dict(
     filetype = 'TIFF',
     tiff_compression = 1,
     convert_bw = False,
-    rotation = 0,
+    rotation = 0, # 0 - no rotation, 1 - 90 counterclockwise, 2 - 180, 3 - 90 clockwise
     iterative_crop = True,
     min_crop_ratio = 0.85,
     max_crop_ratio = 0.90
@@ -53,20 +53,18 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("source", help="Path to the source dir")
     parser.add_argument("target", help="Path to the target dir")
-    parser.add_argument("--film_type", type=int, help="0 - B/W, 1 - color negative, 2 - slide, 3 - crop only")
-    parser.add_argument("--filetype",  help="JPG/TIFF")
-    parser.add_argument("--border_crop", type=int, help="border crop (can be negative)")
-    parser.add_argument("--convert_bw", action=argparse.BooleanOptionalAction, help="Convert to B/W")
-    parser.add_argument("--rotation", type=int, help="0 - no rotation, 1 - 90 counterclockwise, 2 - 180, 3 - 90 clockwise")
+
+    for key, value  in default_settings.items():
+        if type(value) == bool:
+            parser.add_argument(f"--{key}", action=argparse.BooleanOptionalAction)
+        else:
+            parser.add_argument(f"--{key}", type=type(value))
+
     args = parser.parse_args()
     defined_args = {k:v for k,v in args._get_kwargs() if v is not None and k not in {'source', 'target'}}
 
     # merge settings
     default_settings = default_settings | defined_args
-
-    #for dirpath, dirnames, filenames in os.walk(args.source):
-    #    for filename in filenames:
-    #        process_file([filename, dirpath, args.target, default_settings])
 
     with multiprocessing.Manager() as manager:
         with multiprocessing.Pool(4) as pool:
